43af575b418739649c6eaf581062596c5c9ff1d7
hazeem5z <47551501+hazeem5z@users.noreply.github.com>
2019-02-12 11:52:22 +0530

    Update greatest3nos.py

--- greatest3nos.py
+++ greatest3nos.py
@@ -1,10 +1,11 @@
+# your code goes here
 inputs=input().split()
 fn=int(inputs[0])
 sn=int(inputs[1])
 tn=int(inputs[2])
-if fn>sn&fn>tn:
+if fn>sn and fn>tn:
 	print(fn)
-elif sn>fn&sn>tn:
+elif sn>fn and sn>tn:
 	print(sn)
 else:
 	print(tn)
