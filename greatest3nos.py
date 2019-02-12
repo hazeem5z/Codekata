07a9321805d0804b4caab57d57d6f472e10ee66f
hazeem5z <47551501+hazeem5z@users.noreply.github.com>
2019-02-12 11:53:57 +0530

    Update greatest3nos.py

--- greatest3nos.py
+++ greatest3nos.py
@@ -3,9 +3,9 @@ inputs=input().split()
 fn=int(inputs[0])
 sn=int(inputs[1])
 tn=int(inputs[2])
-if fn>sn and fn>tn:
+if fn>=sn and fn>=tn:
 	print(fn)
-elif sn>fn and sn>tn:
+elif sn>=fn and sn>=tn:
 	print(sn)
 else:
 	print(tn)
